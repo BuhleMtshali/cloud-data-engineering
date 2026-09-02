4200702bcd54e847638876c57aec08f5913013d3
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-02 13:01:32 +0200

    Fix typo in timer function reference and improve calculator app structure

--- 01-Tech-Foundations/Day-06-python-basics/CLI_calculator.py
+++ 01-Tech-Foundations/Day-06-python-basics/CLI_calculator.py
@@ -4,8 +4,20 @@ import threading
 # WELCOME MESSAGE
 print("=========== 🧾 MINI CALCULATOR 🧺 ===========")
 
-
+# CREATING THE FUNCTION
+def calculator_app():
+    
+    #STARTING THE WHILE LOOP
+    while True:
+        print("loop running")
+        
+        
+        
+    #CLOSING THE LOOP
+    runAgain = input("==== 💡 Wanna run the calculor again? (yes/no): ").lower()
+    if runAgain != "yes":
+        print("===== ⛳️ Thank you For Trying My Mini Calculator 🎲 =====")
 
 # CALLING THE TIMER FUNCTION
-timer = threading.Timer(2, calculato_app)
+timer = threading.Timer(2, calculator_app)
 timer.start()
