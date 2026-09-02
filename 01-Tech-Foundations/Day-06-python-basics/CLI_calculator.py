5c9448482e2db05ca2012d4e33f493157b00fd5b
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-02 12:47:44 +0200

    Add initial implementation of CLI calculator with timer functionality

---
+++ 01-Tech-Foundations/Day-06-python-basics/CLI_calculator.py
@@ -0,0 +1,11 @@
+# IMPORTING TIMER
+import threading
+
+# WELCOME MESSAGE
+print("=========== 🧾 MINI CALCULATOR 🧺 ===========")
+
+
+
+# CALLING THE TIMER FUNCTION
+timer = threading.Timer(2, calculato_app)
+timer.start()
