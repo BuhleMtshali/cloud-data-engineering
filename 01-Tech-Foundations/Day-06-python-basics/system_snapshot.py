06b98ab70a510aaab2c33c0c88f7afd8680df630
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-02 13:18:24 +0200

    Enhance system_snapshot.py to include user and home directory information in output

--- 01-Tech-Foundations/Day-06-python-basics/system_snapshot.py
+++ 01-Tech-Foundations/Day-06-python-basics/system_snapshot.py
@@ -5,3 +5,7 @@ print("===== 🐧 SYSTEM SNAPSHOT =====")
 user = os.getenv("USER")
 home = os.getenv("HOME")
 current_directory = os.getcwd()
+
+print(f"🙋🏻‍♀️ User: {user}")
+print(f"🏡 Home: {home}")
+print(f"🗂️ Current Directory: {current_directory}")
