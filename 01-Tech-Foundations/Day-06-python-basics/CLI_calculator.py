948916ff748e9b925cf0e15a4008e546a50656b2
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-02 13:06:36 +0200

    Implement operator handling in calculator_app function

--- 01-Tech-Foundations/Day-06-python-basics/CLI_calculator.py
+++ 01-Tech-Foundations/Day-06-python-basics/CLI_calculator.py
@@ -13,7 +13,24 @@ def calculator_app():
         operator = input("🧩 Choose an operator (+, /, *, -): ")
         second_number = float(input("🧺 Enter your second number: "))
         
-        
+        match operator:
+            case "+":
+                output = first_number + second_number
+                print(f"Output: {output}")
+            case "-":
+                output = first_number - second_number
+                print(f"Output: {output}")
+            case "*":
+                output = first_number * second_number
+                print(f"Output: {output}")
+            case "/":
+                if second_number == 0:
+                    print("🚫 Cannot divide by 0")
+                else:
+                    output = first_number / second_number
+                    print(f"Output: {output}")
+            case _:
+                print("‼️ Invalid Operators")
         
     #CLOSING THE LOOP
     runAgain = input("==== 💡 Wanna run the calculor again? (yes/no): ").lower()
