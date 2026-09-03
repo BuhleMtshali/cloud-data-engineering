286ce0c0677579b4a1ca68458fd96c6b7d3644d9
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-03 14:48:59 +0200

    Refactor login_validator function to separate user validation logic and improve structure

--- 01-Tech-Foundations/Day-07-python-control-flow/login_validator.py
+++ 01-Tech-Foundations/Day-07-python-control-flow/login_validator.py
@@ -5,35 +5,36 @@ import threading
 # WELCOME MESSAGE
 print("================ 🤳🏽 MINI LOGIN VALIDATOR ⛔️ =================")
 
+#SIMULATED USER DATABASE
+USER_DATABASE = {
+    "admin": "Secrete123!",
+    "alice": "Wonderland99",
+    "bob": "Builder456"
+}
+    
+
+# VALIDATING IF THE REGIISTERED USER IS THE LOGGING IN
+def login_check():
+    print("------ 🗂️ Let's Validate 🐝 ------")
+    username = input("Enter username: ").strip().lower()
+    password = input("Enter password: ").strip()
+
+        #CHECK IF THE USERNAME EXISTS
+    if username in USER_DATABASE:
+         #CHECKING IF THE PASSWORD MATCHES
+        if USER_DATABASE[username] == password:
+            print(f"----- ✅ Login successful! Welcome back {username}!")
+            return True
+        else:
+            print(f"----- 🆘 ERROR: INCORRECT password.")
+    else:
+        print("----- ⛔️ ERROR: USERNAME NOT FOUND ⛔️ ------")
+
 # STARTING FUNCTION
 def login_validator():
     
-    #SIMULATED USER DATABASE
-    USER_DATABASE = {
-        "admin": "Secrete123!",
-        "alice": "Wonderland99",
-        "bob": "Builder456"
-    }
-    
      # STARTING WHILE LOOP
     while True:
-        
-        # VALIDATING IF THE REGIISTERED USER IS THE LOGGING IN
-        def login_check():
-            print("------ 🗂️ Let's Validate 🐝 ------")
-            username = input("Enter username: ").strip().lower()
-            password = input("Enter password: ").strip()
-
-            #CHECK IF THE USERNAME EXISTS
-            if username in USER_DATABASE:
-                #CHECKING IF THE PASSWORD MATCHES
-                if USER_DATABASE[username] == password:
-                    print(f"----- ✅ Login successful! Welcome back {username}!")
-                    return True
-                else:
-                    print(f"----- 🆘 ERROR: INCORRECT password.")
-            else:
-                print("----- ⛔️ ERROR: USERNAME NOT FOUND ⛔️ ------")
         
             #CLOSING THE LOOP
             login_again = input("====== ⛔️ Wanna check-in someone else? (yes/no): ").lower()
