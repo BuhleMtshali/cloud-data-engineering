d366f71fd967cdf7334dcf192c0710bd78380342
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-03 14:21:38 +0200

    Refactor login_validator function to improve structure and readability

--- 01-Tech-Foundations/Day-07-python-control-flow/login_validator.py
+++ 01-Tech-Foundations/Day-07-python-control-flow/login_validator.py
@@ -5,8 +5,17 @@ import threading
 # WELCOME MESSAGE
 print("================ 🤳🏽 MINI LOGIN VALIDATOR ⛔️ =================")
 
-
-
+# STARTING FUNCTION
+def login_validator():
+    
+     # STARTING WHILE LOOP
+    while True:
+        
+        
+        #CLOSING THE LOOP
+        login_again = input("====== ⛔️ Wanna check-in someone else? (yes/no): ")
+        if login_again != "yes":
+            print("======== 🐧 THANK YOU, BYE 🆘 =======")
 
 
 # CALLING THE TIMER FUNCTION
