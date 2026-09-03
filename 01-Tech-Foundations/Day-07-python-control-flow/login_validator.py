3c611ecc0f8775f0864c184f303f12dd2c7612d4
Buhle <mtshalizanobuhle7@gmail.com>
2026-09-03 12:25:11 +0200

    Add initial implementation of mini login validator with timer functionality

---
+++ 01-Tech-Foundations/Day-07-python-control-flow/login_validator.py
@@ -0,0 +1,14 @@
+# IMPORTING THE TIMER FUNCTION
+
+import threading
+
+# WELCOME MESSAGE
+print("================ 🤳🏽 MINI LOGIN VALIDATOR ⛔️ =================")
+
+
+
+
+
+# CALLING THE TIMER FUNCTION
+timer = threading.Timer(3, login_validator)
+timer.start()
